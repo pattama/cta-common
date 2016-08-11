a2780941d247c591db56258ddb85e6a42cdd1f91
Adnane SAGHIR <adnane.saghir@thomsonreuters.com>
2016-08-11 17:34:59 +0200

    moved sample

--- samples/validate/all.js
+++ samples/validate/all.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const assert = require('chai').assert;
-const validate = require('cta-tools').validate;
+const validate = require('../../lib').validate;
 let result;
 
 result = validate('abc', 'string'); assert(result.isValid);

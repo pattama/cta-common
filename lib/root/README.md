a3a2867c62521f83400246ffcd19b266e61b8267
Adnane SAGHIR <adnane.saghir@thomsonreuters.com>
2016-11-21 14:38:13 +0100

    updated docs & samples

--- lib/root/README.md
+++ lib/root/README.md
@@ -12,26 +12,32 @@ const root = require('cta-common').root('cta-app-myapp');
 
 This will return the root path of your application whenever the structure of your environment
 
-* eg. of a standalone app, this will return `var/www/cta-app-myapp` 
+# Examples
+
+* In case of a standalone app: 
 ````
 var/www/cta-app-myapp
 var/www/cta-app-myapp/node_modules/cta-common
 ````
+It will return `var/www/cta-app-myapp`
 
-* eg. of an aggregated app, this will return `var/www/cta-app-myapp` 
+* In case of an app inside an aggregated app: 
 ````
-var/www/cta-app-myapp
-var/www/cta-app-myapp/node_modules/cta-app-someotherapp/node_modules/cta-common
+var/www/cta-app-aggregated
+var/www/cta-app-aggregated/node_modules/cta-app-myapp/node_modules/cta-common
 ````
+It will return `var/www/cta-app-aggregated/node_modules/cta-app-myapp`
 
-* eg. of a 'all inside node_modules' structure, this will return `var/www/cta/node_modules/cta-app-myapp` 
+* In case of all is inside node_modules: 
 ````
 var/www/cta/node_modules/cta-app-myapp
 var/www/cta/node_modules/cta-common
 ````
+It will return `var/www/cta/node_modules/cta-app-myapp`
 
-* eg. of a 'all inside node_modules' structure with many node_modules folders, this will return `var/www/cta/node_modules/cta-oss/node_modules/cta-app-myapp` 
+* In case of all is inside node_modules, with many node_modules folders: 
 ````
 var/www/cta/node_modules/cta-oss/node_modules/cta-app-myapp
 var/www/cta/node_modules/cta-oss/node_modules/cta-common
 ````
+It will return `var/www/cta/node_modules/cta-oss/node_modules/cta-app-myapp`
--- test/lib/root/index.test.js
+++ test/lib/root/index.test.js
@@ -5,38 +5,38 @@ const o = require('../../common');
 describe('Root', function() {
   context('when it is a standalone application', function() {
     /**
-     * var/www/cta-app-standalone
-     * var/www/cta-app-standalone/node_modules/cta-common
+     * var/www/cta-app-myapp
+     * var/www/cta-app-myapp/node_modules/cta-common
      */
     it('should look for application root in __dirname', function() {
-      const root = o.root('cta-app-standalone', 'var/www/cta-app-standalone/node_modules/cta-common/lib/root').replace(/\\/g, '/');
-      o.assert.strictEqual(root, 'var/www/cta-app-standalone');
+      const root = o.root('cta-app-myapp', 'var/www/cta-app-myapp/node_modules/cta-common/lib/root').replace(/\\/g, '/');
+      o.assert.strictEqual(root, 'var/www/cta-app-myapp');
     });
   });
-  context('when it is an aggregated application', function() {
+  context('when it is inside an aggregated application', function() {
     /**
-     * var/www/cta-app-aggregated
-     * var/www/cta-app-aggregated/node_modules/cta-app-standalone/node_modules/cta-common
+     * var/www/cta-app-aggregated/node_modules/cta-app-myapp
+     * var/www/cta-app-aggregated/node_modules/cta-app-myapp/node_modules/cta-common
      */
     it('should look for application root in __dirname', function() {
-      const root = o.root('cta-app-aggregated', 'var/www/cta-app-aggregated/node_modules/cta-app-standalone/node_modules/cta-common/lib/root').replace(/\\/g, '/');
-      o.assert.strictEqual(root, 'var/www/cta-app-aggregated');
+      const root = o.root('cta-app-myapp', 'var/www/cta-app-aggregated/node_modules/cta-app-myapp/node_modules/cta-common/lib/root').replace(/\\/g, '/');
+      o.assert.strictEqual(root, 'var/www/cta-app-aggregated/node_modules/cta-app-myapp');
     });
   });
   context('when all is inside node_modules', function() {
     /**
-     * var/www/node_modules/cta-app
+     * var/www/node_modules/cta-myapp
      * var/www/node_modules/cta-common
      */
     it('should concatenate node_modules path and application folder', function() {
-      const root = o.root('cta-app', 'var/www/node_modules/cta-common/lib/root').replace(/\\/g, '/');
-      o.assert.strictEqual(root, 'var/www/node_modules/cta-app');
+      const root = o.root('cta-myapp', 'var/www/node_modules/cta-common/lib/root').replace(/\\/g, '/');
+      o.assert.strictEqual(root, 'var/www/node_modules/cta-myapp');
     });
   });
   context('when it can\'t find application root', function() {
     it('should throw an error', function() {
       try {
-        const root = o.root('cta-app', 'var/www/cta-common/lib/root');
+        const root = o.root('cta-myapp', 'var/www/cta-common/lib/root');
         o.assert.fail('should throw an error!');
       } catch (e) {
         o.assert.ok('ok');
